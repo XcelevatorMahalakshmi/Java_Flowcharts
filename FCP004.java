a2e663a021a50d7ac555c043acc8c6c064a91083
XcelevatorMahalakshmi <mahalakshmi.2516@xcelevate.org>
2025-07-31 11:47:49 +0530

    Add files via upload

---
+++ FCP004.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class FCP004{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+int c=(a+b);
+int d=(a-b);
+int e=(a*b);
+int f=(a/b);
+int g=(a%b);
+
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+}
+}
