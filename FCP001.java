b03038f88d22115c7a2ddada744218210593c837
XcelevatorMahalakshmi <mahalakshmi.2516@xcelevate.org>
2025-07-31 11:37:13 +0530

    Add files via upload

---
+++ FCP001.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FCP001{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+int date=scan.nextInt();
+scan.nextLine();
+String month=scan.nextLine();
+int year=scan.nextInt();
+System.out.println(date);
+System.out.println(month);
+System.out.println(year);
+}
+}
+
+
+
+
