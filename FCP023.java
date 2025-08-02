2623bb08d9171269e7836f1a41e35ba223676b62
XcelevatorMahalakshmi <mahalakshmi.2516@xcelevate.org>
2025-08-02 16:05:07 +0530

    Add files via upload

---
+++ FCP023.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class FCP023{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+scan.nextLine();
+String name=scan.nextLine();
+for(int i=1;i<=a;i=i+1){
+System.out.println(name);
+}
+}
+}
